cd6b89b5ae59531fd6245ec678255fca546e7d25
Daniel <jos.god.mel.10@gmail.com>
2023-11-25 22:20:14 -0600

    Agregar archivos de proyecto.

---
+++ SliderWord/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace SliderWord
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            rotateThisObject.Text = message.Text;
+        }
+
+        private async void sliderRotate_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            await rotateThisObject.RotateTo((sliderRotate.Value * 360));
+            degrees.Text = (sliderRotate.Value * 360) + "°";
+        }
+
+        private void sliderImg_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            imagePine.RotateTo((sliderImg.Value * 360));
+        }
+    }
+}
